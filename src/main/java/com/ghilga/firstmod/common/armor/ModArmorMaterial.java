951a2cf072854c839e2b97ce08f7914e6023c98d
Ghilga <Ghilga@users.noreply.github.com>
2020-09-02 21:44:16 -0300

    Created Enum class as a base for new armors

---
+++ src/main/java/com/ghilga/firstmod/common/armor/ModArmorMaterial.java
@@ -0,0 +1,77 @@
+package com.ghilga.firstmod.common.armor;
+
+import com.ghilga.firstmod.FirstMod;
+import com.ghilga.firstmod.util.RegistryHandler;
+import net.minecraft.client.tutorial.Tutorial;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+import java.util.zip.DeflaterInputStream;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+
+    RUBY(FirstMod.MOD_ID + ":ruby", 36, new int[] {5, 9, 7, 4}, 18,
+            SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 2.0f, () -> { return Ingredient.fromItems(RegistryHandler.RUBY.get()); });
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[] {11, 16, 15, 13};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability,
+                     SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial) {
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex() * this.maxDamageFactor];
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.get();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+}
