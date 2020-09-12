e4da4f636b404c1a16cf0b22464ea0d45c0a6cb8
Ghilga <Ghilga@users.noreply.github.com>
2020-09-12 19:09:15 -0300

    created log for mob drops also deactivated picked items log and used try catches in all logs to prevent crashes

--- src/main/java/com/ghilga/firstmod/util/EventHandler.java
+++ src/main/java/com/ghilga/firstmod/util/EventHandler.java
@@ -3,13 +3,17 @@ package com.ghilga.firstmod.util;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.EntityDamageSource;
+import net.minecraft.util.IndirectEntityDamageSource;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraft.util.text.TextFormatting;
 public class EventHandler {
-    @SubscribeEvent
+
+    //@SubscribeEvent
     public void pickupItem(EntityItemPickupEvent event) {
         PlayerEntity playerEntity = event.getPlayer();
         playerEntity.sendMessage(new StringTextComponent("You picked up " + event.getItem().getName().getString()), playerEntity.getUniqueID());
@@ -18,8 +22,25 @@ public class EventHandler {
     @SubscribeEvent
     public void combatLog(LivingExperienceDropEvent event) {
         Entity playerEntity = event.getAttackingPlayer();
-        playerEntity.sendMessage(new StringTextComponent(
-                event.getEntity().getName().getString() + " died" +
-                "\nDropped " + TextFormatting.DARK_GREEN + event.getDroppedExperience() + " XP"), playerEntity.getUniqueID());
+        try {
+            playerEntity.sendMessage(new StringTextComponent(
+                    event.getEntity().getName().getString() + " died" +
+                            "\nDropped " + TextFormatting.DARK_GREEN + event.getDroppedExperience() + " XP"), playerEntity.getUniqueID());
+        } catch (Exception ex){
+            System.out.println(ex.toString());
+        }
+    }
+
+    @SubscribeEvent
+    public void dropLog(LivingDropsEvent event){
+        Entity killer = event.getSource().getTrueSource();
+        try{
+            for (ItemEntity item : event.getDrops()) {
+                killer.sendMessage(new StringTextComponent("Dropped " + item.getItem().getCount() + " "
+                                    + TextFormatting.YELLOW + item.getName().getString()), killer.getUniqueID());
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
